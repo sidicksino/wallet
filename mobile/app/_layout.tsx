0d3e1ebd0a72eb0456a1c522a2b7c100e8ff1939
sidicksino <sidickabdoulayesino1@gmail.com>
2025-07-31 17:15:16 +0200

    Start wallet app frontend et backend

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import SafeScreen from "../components/SafeScreen";
+
+export default function RootLayout() {
+  return (
+    <SafeScreen>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </SafeScreen>
+  );
+}
